7855c4b6073dcb5f23b81ff4e1063d4e4ed95694
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-11-09 22:19:07 +0100

    Update Prediction.py

--- pages/Prediction.py
+++ pages/Prediction.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import time
+from utils import functions.viewer
 
 
 simai_project = st.selectbox(
@@ -62,30 +63,4 @@ if st.button("Predict"):
         time.sleep(5)
         
 if st.button("Download"):
-    import pyvista as pv
-    from stpyvista import stpyvista
-
-    from stpyvista.utils import start_xvfb
-
-    if "IS_XVFB_RUNNING" not in st.session_state:
-      start_xvfb()
-      st.session_state.IS_XVFB_RUNNING = True
-
-    ## Initialize a plotter object
-    plotter = pv.Plotter(window_size=[400,400])
-
-    ## Create a mesh with a cube 
-    mesh = pv.Cube(center=(0,0,0))
-
-    ## Add some scalar field associated to the mesh
-    mesh['myscalar'] = mesh.points[:, 2] * mesh.points[:, 0]
-
-    ## Add mesh to the plotter
-    plotter.add_mesh(mesh, scalars='myscalar', cmap='bwr')
-
-    ## Final touches
-    plotter.view_isometric()
-    plotter.background_color = 'white'
-
-    ## Send to streamlit
-    stpyvista(plotter, key="pv_cube")
+    viewer()
