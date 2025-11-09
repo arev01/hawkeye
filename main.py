775a84f61aa00b0e25dffb5b33848caee9204c9c
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-11-09 13:15:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import time
 
 st.title("👋 Welcome to hawkeye!")
 
@@ -23,3 +24,32 @@ elif submit and email != actual_email and password != actual_password:
     st.sidebar.error("Login failed")
 else:
     pass
+
+project =st.selectbox("Project")
+workspace = st.selectbox("Wokspace")
+choice = st.radio("Choice",
+    [
+    "Upload a new file",
+    "Use existing file",
+    "Generate a new file"]
+)
+
+if choice == "Upload a new file":
+    st.file_uploader("")
+
+elif choice == "Use existing file":
+    st.selectbox("Geometry")
+
+elif choice == "Generate a new file":
+    st.selectbox("Project")
+    st.selectbox("Workspace")
+
+    st.slider("Latent parameters")
+
+st.text_input("Boundary conditions")
+
+with st.button("Predict"):
+    st.spinner("Wait for it..."):
+    time.sleep(5)
+
+st.button("Download", disabled=True)
