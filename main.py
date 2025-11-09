db58ec1399b9844cb57df8946445049325859e68
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-11-09 21:01:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,45 @@
 import streamlit as st
 import time
 
+from contextlib import contextmanager
+
+HORIZONTAL_STYLE = """
+<style class="hide-element">
+    /* Hides the style container and removes the extra spacing */
+    .element-container:has(.hide-element) {
+        display: none;
+    }
+    /*
+        The selector for >.element-container is necessary to avoid selecting the whole
+        body of the streamlit app, which is also a stVerticalBlock.
+    */
+    div[data-testid="stVerticalBlock"]:has(> .element-container .horizontal-marker) {
+        display: flex;
+        flex-direction: row !important;
+        flex-wrap: wrap;
+        gap: 0.5rem;
+        align-items: baseline;
+    }
+    /* Buttons and their parent container all have a width of 704px, which we need to override */
+    div[data-testid="stVerticalBlock"]:has(> .element-container .horizontal-marker) div {
+        width: max-content !important;
+    }
+    /* Just an example of how you would style buttons, if desired */
+    /*
+    div[data-testid="stVerticalBlock"]:has(> .element-container .horizontal-marker) button {
+        border-color: red;
+    }
+    */
+</style>
+"""
+
+@contextmanager
+def st_horizontal():
+    st.markdown(HORIZONTAL_STYLE, unsafe_allow_html=True)
+    with st.container():
+        st.markdown('<span class="hide-element horizontal-marker"></span>', unsafe_allow_html=True)
+        yield
+
 st.title("👋 Welcome to hawkeye!")
 
 st.markdown("A Graphical User Interface (GUI) built around [PySimAI](https://simai.docs.pyansys.com).")
@@ -59,8 +98,9 @@ elif choice == "Generate a new file":
 
 st.text_input("Boundary conditions")
 
-if st.button("Predict"):
-    with st.spinner("Wait for it..."):
-        time.sleep(5)
-
-st.button("Download", disabled=True)
+with st_horizontal():
+    if st.button("Predict"):
+        with st.spinner("Wait for it..."):
+            time.sleep(5)
+            
+    st.button("Download", disabled=True)
