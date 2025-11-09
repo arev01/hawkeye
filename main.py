81e9eb38b29851bd3ec49cf0476bbbd1157d78d4
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-11-09 13:38:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -25,25 +25,33 @@ elif submit and email != actual_email and password != actual_password:
 else:
     pass
 
-project =st.selectbox("Project")
-workspace = st.selectbox("Wokspace")
-choice = st.radio("Choice",
-    [
-    "Upload a new file",
-    "Use existing file",
-    "Generate a new file"]
+project =st.selectbox(
+    "Project",
+    ("Email", "Home phone", "Mobile phone"),
+)
+workspace = st.selectbox(
+    "Workspace",
+    ("Email", "Home phone", "Mobile phone"),
+)
+choice = st.radio(
+    "Choice",
+    (
+        "Upload a new file",
+        "Use existing file",
+        "Generate a new file"
+    ),
 )
 
 if choice == "Upload a new file":
     st.file_uploader("")
 
 elif choice == "Use existing file":
-    st.selectbox("Geometry")
+    st.selectbox(
+        "Geometry",
+        ("Email", "Home phone", "Mobile phone"),
+    )
 
 elif choice == "Generate a new file":
-    st.selectbox("Project")
-    st.selectbox("Workspace")
-
     st.slider("Latent parameters")
 
 st.text_input("Boundary conditions")
