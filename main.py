4428886a6c3d9029daf366c361176695a244cc74
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-11-10 08:54:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,25 +4,11 @@ st.title("👋 Welcome to hawkeye!")
 
 st.markdown("A Graphical User Interface (GUI) built around [PySimAI](https://simai.docs.pyansys.com).")
 
-actual_email = "email"
-actual_password = "password"
 
-# Insert a form in the sidebar
-with st.sidebar.form("Login"):
-    st.markdown("#### Enter your credentials")
-    
-    tenant = st.text_input("Tenant")
-    email = st.text_input("Email")
-    password = st.text_input("Password", type="password")
-    submit = st.form_submit_button("Login")
-
-if submit and ( email == actual_email and password == actual_password ):
-    # If the form is submitted and the email and password are correct,
-    # Display a success message
-    st.sidebar.success("Login successful")
-    
-elif submit and ( email != actual_email or password != actual_password ):
-    st.sidebar.error("Login failed")
-    
-else:
-    pass
+st.markdown(
+    """
+    👈 Write something here
+    👈 Write something here
+    👈 Write something here
+    """
+)
