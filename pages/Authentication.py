f4afac50b1aaad94c0414138b1f1e0f801be1368
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-11-10 09:00:02 +0100

    Update Authentication.py

--- pages/Authentication.py
+++ pages/Authentication.py
@@ -1,3 +1,6 @@
+import streamlit as st
+import ansys.simai.core
+
 actual_email = "email"
 actual_password = "password"
 
@@ -5,18 +8,29 @@ actual_password = "password"
 with st.form("Login"):
     st.markdown("#### Enter your credentials")
     
-    tenant = st.text_input("Tenant")
-    email = st.text_input("Email")
-    password = st.text_input("Password", type="password")
+    my_organization = st.text_input("Organization", placeholder="eg. ansys")
+    my_username = st.text_input("Username")
+    my_password = st.text_input("Password", type="password")
     submit = st.form_submit_button("Login")
 
-if submit and ( email == actual_email and password == actual_password ):
-    # If the form is submitted and the email and password are correct,
-    # Display a success message
-    st.success("Login successful")
-    
-elif submit and ( email != actual_email or password != actual_password ):
-    st.error("Login failed")
+if submit:
+    try:
+        simai = ansys.simai.core.SimAIClient(
+            organization=my_organization,
+            credentials={
+                # neither of these are required, but if they are missing you will be
+                # prompted to input them
+                "username": my_username,
+                "password": my_password,
+            },
+        )
+        simai.wait()        
+        st.success("Login successful")
+
+        st.session_state["simai"] = simai
+
+    except:
+        st.error("Login failed")
     
 else:
     pass
