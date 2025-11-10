c4fbf8a48e14ab4b093aaa8ad68b48761794c78c
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-11-10 09:28:55 +0100

    Update 🔐 Authentication.py

--- pages/🔐 Authentication.py
+++ pages/🔐 Authentication.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import ansys.simai.core
+from ansys.simai.core import SimAIClient
 
 
 st.title("🔐 Authentication")
@@ -8,26 +8,26 @@ st.title("🔐 Authentication")
 with st.form("Login"):
     st.markdown("#### Enter your credentials")
     
-    my_organization = st.text_input("Organization", placeholder="eg. ansys")
-    my_username = st.text_input("Username")
-    my_password = st.text_input("Password", type="password")
+    ORG_NAME = st.text_input("Organization", placeholder="eg. ansys")
+    USERNAME = st.text_input("Username")
+    PASSWORD = st.text_input("Password", type="password")
     submit = st.form_submit_button("Login")
 
 if submit:
     try:
-        simai = ansys.simai.core.SimAIClient(
-            organization=my_organization,
+        client = SimAIClient(
+            organization=ORG_NAME,
             credentials={
                 # neither of these are required, but if they are missing you will be
                 # prompted to input them
-                "username": my_username,
-                "password": my_password,
+                "username": USERNAME,
+                "password": PASSWORD,
             },
         )
         simai.wait()        
         st.success("Login successful")
 
-        st.session_state["simai"] = simai
+        st.session_state["client"] = client
 
     except:
         st.error("Login failed")
